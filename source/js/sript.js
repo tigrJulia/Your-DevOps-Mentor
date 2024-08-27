e6a49ff86e91998a340bf4ba63046870c4aa5012
Julia <154320351+tigrJulia@users.noreply.github.com>
2024-08-27 12:26:10 +0300

    Add files via upload

---
+++ source/js/sript.js
@@ -0,0 +1,18 @@
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
+
+$(document).ready(function(){
+	$('#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
